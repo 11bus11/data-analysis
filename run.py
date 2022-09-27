c00de3b02dedf231a172f8a1a957afecd224fc3f
11bus11 <02linfor01@gmail.com>
2022-09-27 15:07:47 +0200

    making it possible to do 2 comparisons
    Currentl having a bit of trouble extending the list

--- run.py
+++ run.py
@@ -23,12 +23,19 @@ percentage = []
 
 datalist = []
 
-result = 0
 elements = []
 temp = 0
 i = 0
 
+def prepare():
+    polldata = input()
+    print(polldata)
+    datalist = polldata.split(",")
+    print(datalist)
+    return datalist
+
 def convert_to_int():
+    datalist = prepare()
     i = 0
     while i < len(datalist):
         temp = datalist[i]
@@ -37,34 +44,48 @@ def convert_to_int():
     print(datalist)
     return datalist
 
-def analyse(intdata):
-    print(intdata)
-    percentage = []
-    result = []
+def analyse():
+    intlist = convert_to_int()
+    elements = []
+    percent = 0
+    name = input("what kind of data is it?\n")
     print("what do you want?")
     print("Write the elements you want to compare to the number of answers")
     elements = input().split(",")
+    print(elements, "elements")
+    print(intlist, "int")
     x = 0
     while x < len(elements):
+        result = [name]
+        temp = ["temp"]
         if elements[x] == "yes":
-            result = result.append(intdata[1]/intdata[0])
+            percent = intlist[1]/intlist[0]
+            temp[0] = percent
+            result = result.extend(temp)
+            print(percent)
+            print(result)
         elif elements[x] == "no":
-            result = result.append(intdata[2]/intdata[0])
+            percent = intlist[2]/intlist[0]
+            temp[0] = percent
+            result = result.extend(temp)
+            print(percent)
+            print(result)
         elif elements[x] == "none":
-            result = result.append(intdata[3]/intdata[0])
+            percent = intlist[3]/intlist[0]
+            temp[0] = percent
+            result = result + temp
+            print(percent)
         else:
             print("not done")
         x = x + 1
-    print(datalist)
-    print(result)
+    
+    print(intlist)
+    print(result, "RES")
+    return result
 
 def main():
     print("Input your data in this fashion:")
     print("answer amount,yes answers,no answers,none\n")
-    polldata = input()
-    print(polldata)
-    datalist = polldata.split(",")
-    print(datalist)
-    print(analyse(convert_to_int()))
+    print(analyse())
 
 main()
