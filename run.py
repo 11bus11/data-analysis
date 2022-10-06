64d5bb660aaa8a874881e4f43904b453bbd93b49
Erik Vodopivec Forsman <02linfor01@gmail.com>
2022-10-06 16:08:56 +0000

    Fixed scope

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 import gspread
 from google.oauth2.service_account import Credentials
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-CREDS = Credentials.from_service_account_file("creds.json")
+
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("Info-stats")
+SHEET = GSPREAD_CLIENT.open('data_analysis')
 
-no1 = SHEET.worksheet("no1")
+answers = SHEET.worksheet("answers")
+output = SHEET.worksheet("output")
+
+data = answers.get_all_values()
 
 polldata = ""
 percentage = []
